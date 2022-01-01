b1770613de0dc3f04ab1b7a1956216605481cfe5
Frank <2521647197@qq.com>
2022-01-01 20:52:53 +0800

    deploy

---
+++ precache-manifest.f1f30e469ea478e0fd0b0a9ed7b26130.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f083eb951a53f121692",
+    "url": "/money-rouoru-website/css/app.205ff8e9.css"
+  },
+  {
+    "revision": "ffa080bda70f725e01bdd38b8a9d199b",
+    "url": "/money-rouoru-website/index.html"
+  },
+  {
+    "revision": "0f083eb951a53f121692",
+    "url": "/money-rouoru-website/js/app.b66b9691.js"
+  },
+  {
+    "revision": "3ac75f9df9f490ced0da",
+    "url": "/money-rouoru-website/js/chunk-vendors.9f2bb0b7.js"
+  },
+  {
+    "revision": "f31b467887377693c5d8d121bf699a7f",
+    "url": "/money-rouoru-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-rouoru-website/robots.txt"
+  }
+]);
